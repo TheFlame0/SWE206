f64463c24f95226db344acd8aa4deb98fefe0096
Hamza Ba'aqeel <h.m.b.2002@hotmail.com>
2023-02-14 19:37:22 +0300

    Added Selection Sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,4 +2,21 @@ public class Sorter {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    static void swap(int[] array, int a, int b) {
+		int temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;
+	}
+
+	public static int[] selectionSort(int[] array) {
+		int min;
+		for (int i = 0; i < array.length; i++) {
+			min = i;
+			for (int j = i; j < array.length; j++) {
+				if (array[min] > array[j])
+					min = j;
+			}
+			swap(array, i, min);
+		}
+		return array;
 }
